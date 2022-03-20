ac39cbbec87a077bce2d027eca4eec32ae8f3d0a
swapnil.mishraj <swapnil.mishraj@gmail.com>
2022-03-20 15:35:15 -0400

    use multiple canvas and add listeners to onclick

--- js/custom.js
+++ js/custom.js
@@ -1,93 +1,150 @@
 var cpu_data;
-var time;
 var disk_data;
+
+
+var disk_perf = [90, 10, 20, 50, 0, 70]
+let cpu_perf;
+let time;
+
+let myChart;
+let myChartCPU;
 
 var xmlhttp = new XMLHttpRequest();
 var url = "http://127.0.0.1:5500/cpu_usage.json";
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
-xmlhttp.onreadystatechange = function(){
-    if(this.readyState == 4 && (this.status == 200 || this.status == 304)){
-        var data = JSON.parse(this.responseText);
+xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState == 4 && (xmlhttp.status == 200 || xmlhttp.status == 304)){
+        handle_cpu_usage(xmlhttp.responseText);
+    }
+}
+
+var xmlhttp2 = new XMLHttpRequest();
+var url = "http://127.0.0.1:5500/disk_usage.json";
+xmlhttp2.open("GET", url, true);
+xmlhttp2.send();
+xmlhttp2.onreadystatechange=function() {
+    if (xmlhttp2.readyState == 4 && (xmlhttp2.status == 200 || xmlhttp2.status == 304)){
+        handle_disk_usage(xmlhttp2.responseText);
+    }
+}
+
+
+
+function handle_cpu_usage(response) {
+        cpu_data = JSON.parse(response);
         //console.log(data);
-        var time = data.data.map(function(elem){
+        time = cpu_data.data.map(function(elem){
             return elem.time;
         });
         //console.log(time);
-        var perf = data.data.map(function(elem){
+        cpu_perf = cpu_data.data.map(function(elem){
             return elem.perf;
         });
 
-        const ctx = document.getElementById('canvas').getContext('2d');
-        const myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: time,
-                datasets: [{
-                    label: 'cpu perf',
-                    data: perf,
-                    backgroundColor: 'transparent',
-                    borderColor: 'red',
-                    borderWidth: 4
-                },
-                {
-                    label: 'disk perf',
-                    data: perf,
-                    backgroundColor: 'transparent',
-                    borderColor: 'red',
-                    borderWidth: 4
-                }]
-            },
-            options: {
-                elements:{
-                    line:{
-                        tension:0
-                    }
-                },
-                scales: {
-                    yAxes: [{
-                        ticks:{
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-        const ctx2 = document.getElementById('canvas2').getContext('2d');
-        const myChart2 = new Chart(ctx2, {
-            type: 'line',
-            data: {
-                labels: time,
-                datasets: [{
-                    label: 'cpu perf',
-                    data: perf,
-                    backgroundColor: 'transparent',
-                    borderColor: 'red',
-                    borderWidth: 4
-                },
-                {
-                    label: 'disk perf',
-                    data: perf,
-                    backgroundColor: 'transparent',
-                    borderColor: 'red',
-                    borderWidth: 4
-                }]
-            },
-            options: {
-                elements:{
-                    line:{
-                        tension:0
-                    }
-                },
-                scales: {
-                    yAxes: [{
-                        ticks:{
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
+        update_chart();
+}
+
+function handle_disk_usage(response) {
+        disk_data = JSON.parse(response);
+        disk_perf = disk_data.data.map(function(elem){
+            return elem.usage;
         });
 
-    }
+        update_chart2();
+}
+
+function update_chart(){
+    const ctx = document.getElementById('canvas');
+    const myChartCPU = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: time,   
+            datasets: [{
+                label: 'cpu perf',
+                data: cpu_perf,
+                backgroundColor: 'transparent',
+                borderColor: 'red',
+                borderWidth: 4
+            }
+        ]
+        },
+        options: {
+            elements:{
+                line:{
+                    tension:0
+                }
+            },
+            scales: {
+                y: {
+                    max: 100,
+                    min: 0,
+                    ticks: {
+                        stepSize: 10
+                    }
+                }
+            }
+        }
+    });
+
+    ctx.onclick = clickHandlerCPU; 
+
+}
+
+function update_chart2(){
+    const ctx = document.getElementById('canvas2');
+    myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: time,   
+            datasets: [{
+                label: 'disk perf',
+                data: disk_perf,
+                backgroundColor: 'transparent',
+                borderColor: 'blue',
+                borderWidth: 4
+            }]
+        },
+        options: {
+            elements:{
+                line:{
+                    tension:0
+                }
+            },
+            scales: {
+                y: {
+                    max: 100,
+                    min: 0,
+                    ticks: {
+                        stepSize: 10
+                    }
+                }
+            }
+        }
+    });
+    ctx.onclick = clickHandlerDisk; 
+
+}
+
+function clickHandlerDisk(click){
+    console.log('clicked');
+    const points = myChart.getElementsAtEventForMode(click, 'nearest',
+        { intersect:true }, true);
+
+        if(points.length){
+            const firstPoint  = points[0];
+            console.log(points);
+        }
+}
+
+function clickHandlerCPU(click){
+    console.log('clicked');
+    const points = myChart.getElementsAtEventForMode(click, 'nearest',
+        { intersect:true }, true);
+
+        if(points.length){
+            const firstPoint  = points[0];
+            
+            console.log(cpu_data.data[firstPoint.index].processes);
+        }
 }
